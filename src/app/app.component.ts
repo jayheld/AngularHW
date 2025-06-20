2b2c0b443e078b91f61c7f9f235fd8bb03e4cba6
James Heldridge <jamesheldridge@Jamess-MacBook-Pro-3.local>
2025-06-20 12:11:33 -0500

    feat: Add create user form and functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserService } from './user.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 interface User {
   id: number;
@@ -12,7 +13,7 @@ interface User {
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -20,6 +21,7 @@ export class AppComponent implements OnInit {
   title = 'Hello World Angular App';
   
   users: User[] = [];
+  newUser: User = { id: 0, name: '', email: '', role: '' };
 
   currentDate = new Date();
 
@@ -32,6 +34,26 @@ export class AppComponent implements OnInit {
   loadUsers() {
     this.userService.getUsers().subscribe(data => {
       this.users = data;
+    });
+  }
+
+  addUser() {
+    // A real app would have more robust validation
+    if (!this.newUser.name || !this.newUser.email || !this.newUser.role) {
+      alert('Please fill out all fields.');
+      return;
+    }
+    
+    // We don't send an ID, json-server will create it
+    const userToAdd = {
+      name: this.newUser.name,
+      email: this.newUser.email,
+      role: this.newUser.role
+    };
+
+    this.userService.addUser(userToAdd as User).subscribe(() => {
+      this.loadUsers(); // Refresh the list
+      this.newUser = { id: 0, name: '', email: '', role: '' }; // Reset the form
     });
   }
 
